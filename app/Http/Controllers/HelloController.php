0422c3a3794546a560ee5b20ce41d768838f87e4
ito0804takuya <ito0804takuya@gmail.com>
2020-03-01 18:07:36 +0900

    Add FormRequest

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,18 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Request\HelloRequest;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
-
-        
         return view('hello.index');
     }
 
-    public function post(Request $request) {
-        $msg = $request->msg;
-        $data = [ 'msg' => 'こんにちは' . $msg . 'さん'];
+    public function post(HelloRequest $request) {
         return view('hello.index', $data);
     }
 }
